0323e7a8dca0555fd87046251cc0d763d7ad944e
Daniel O'Brien <dan@dobs.org>
2017-02-24 20:08:40 -0500

    Initial RSpec support.

--- autographql.gemspec
+++ autographql.gemspec
@@ -18,5 +18,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency 'rails', '~> 5.0.1'
 
+  s.add_development_dependency 'factory_girl_rails'
+  s.add_development_dependency 'rspec-rails'
   s.add_development_dependency 'sqlite3'
 end
---
+++ spec/authgraphql_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Autographql do
+  it 'should be a `Module`' do
+    expect(described_class).to be_a Module
+  end
+end
+
+describe Dummy do
+  it 'should be a `Module`' do
+    expect(described_class).to be_a Module
+  end
+end
